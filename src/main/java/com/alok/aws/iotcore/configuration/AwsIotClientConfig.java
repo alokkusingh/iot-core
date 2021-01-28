712970ee4c99383a72b0671ac6d17670d33aca57
alokkusingh <alok.ku.singh@gmail.com>
2021-01-28 16:23:32 +0530

    Added Jenkinsfile

--- src/main/java/com/alok/aws/iotcore/configuration/AwsIotClientConfig.java
+++ src/main/java/com/alok/aws/iotcore/configuration/AwsIotClientConfig.java
@@ -1,10 +1,8 @@
 package com.alok.aws.iotcore.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.services.iot.IotClient;
 
 @Configuration
@@ -14,22 +12,22 @@ public class AwsIotClientConfig {
     //@Value("${aws.region:ap-south-1}")
     //private String awsRegion;
 
-    @Value("${aws.iot-core-accessKey:test}")
+    /*@Value("${aws.iot-core-accessKey:test}")
     private String awsAccessKey;
 
     @Value("${aws.iot-core-secret:test}")
-    private String awsSecret;
+    private String awsSecret;*/
 
     @Bean
     public IotClient iotClientConfigBean() {
         //return IotClient.create();
         return IotClient.builder()
                 //.region(Region.of(awsRegion))
-                .credentialsProvider(awsCredentialsProvider())
+               // .credentialsProvider(awsCredentialsProvider())
                 .build();
     }
 
-    @Bean
+    /*@Bean
     public AwsCredentialsProvider awsCredentialsProvider() {
         return AwsCredentialsProviderChain.builder()
                 .addCredentialsProvider(SystemPropertyCredentialsProvider.create())
@@ -39,5 +37,5 @@ public class AwsIotClientConfig {
                 )
                 .build();
         //return SystemPropertyCredentialsProvider.create();
-    }
+    }*/
 }
